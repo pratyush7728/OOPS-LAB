0b2b09309889115fe4432ef9e6d7bcddf577811e
pratyush7728 <94605742+pratyush7728@users.noreply.github.com>
2023-07-26 14:38:12 +0530

    Create squareofno.cpp

---
+++ squareofno.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cout<<"This is output\n";
+	cout<<"Enter a number\n";
+	cin>>i;
+	cout<<i<<" Squared is "<<i*i<<"\n";
+	return 0;
+}
